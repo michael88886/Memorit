97d9e904f1a99793399d0c81c3b3aecb5001beb2
MICA17 <mk17@192-168-1-13.tpgi.com.au>
2019-06-02 16:16:08 +1000

    Initial commit

---
+++ MemoIt/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  MemoIt
+//
+//  Created by MICA17 on 2/6/19.
+//  Copyright © 2019 MC2. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
